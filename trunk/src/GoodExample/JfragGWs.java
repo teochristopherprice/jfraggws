b87224903c1954a7eac7f18ea66e652936b6cd5e
prichr27@gmail.com <prichr27@gmail.com@74befb2e-3a26-938d-26cd-2f3470803c67>
2013-12-20 12:36:29 +0000

    Removed license information that carried over from an example that was not used.

--- trunk/src/GoodExample/JfragGWs.java
+++ trunk/src/GoodExample/JfragGWs.java
@@ -10,15 +10,6 @@ import java.util.HashMap;
 
 import sun.misc.BASE64Encoder;
 
-/**
- * Title:        Sample Server
- * Description:  This utility will accept input from a socket, posting back to the socket before closing the link.
- * It is intended as a template for coders to base servers on. Please report bugs to brad at kieser.net
- * Copyright:    Copyright (c) 2002
- * Company:      Kieser.net
- * @author B. Kieser
- * @version 1.0
- */
 
 public class JfragGWs {
   public static int NumberOfClientsCreates;
